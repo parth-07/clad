0b9d07b51f38d556028ab940bec7aaecb79a3cbd
Parth <partharora99160808@gmail.com>
2021-03-16 22:48:08 +0530

    Add tests for differentiation calls with parantheses

--- test/ForwardMode/Pointers.C
+++ test/ForwardMode/Pointers.C
@@ -13,7 +13,6 @@ public:
   double memFn(double i,double j) {
     return i*i*j*x + j*j*y;
   }
-
   // CHECK: double memFn_darg0(double i, double j) {
   // CHECK-NEXT:     double _d_i = 1;
   // CHECK-NEXT:     double _d_j = 0;
@@ -90,7 +89,94 @@ public:
   // CHECK-NEXT:     double &_t4 = this->y;
   // CHECK-NEXT:     return ((_d_i * i + i * _d_i) * j + _t0 * _d_j) * _t2 + _t1 * 0. + (_d_j * j + j * _d_j) * _t4 + _t3 * 0.;
   // CHECK-NEXT: }
-
+  
+  // CHECK: double memFn_darg0(double i, double j) {
+  // CHECK-NEXT:     double _d_i = 1;
+  // CHECK-NEXT:     double _d_j = 0;
+  // CHECK-NEXT:     double _t0 = i * i;
+  // CHECK-NEXT:     double _t1 = _t0 * j;
+  // CHECK-NEXT:     double &_t2 = this->x;
+  // CHECK-NEXT:     double _t3 = j * j;
+  // CHECK-NEXT:     double &_t4 = this->y;
+  // CHECK-NEXT:     return ((_d_i * i + i * _d_i) * j + _t0 * _d_j) * _t2 + _t1 * 0. + (_d_j * j + j * _d_j) * _t4 + _t3 * 0.;
+  // CHECK-NEXT: }
+  
+  // CHECK: double memFn_darg0(double i, double j) {
+  // CHECK-NEXT:     double _d_i = 1;
+  // CHECK-NEXT:     double _d_j = 0;
+  // CHECK-NEXT:     double _t0 = i * i;
+  // CHECK-NEXT:     double _t1 = _t0 * j;
+  // CHECK-NEXT:     double &_t2 = this->x;
+  // CHECK-NEXT:     double _t3 = j * j;
+  // CHECK-NEXT:     double &_t4 = this->y;
+  // CHECK-NEXT:     return ((_d_i * i + i * _d_i) * j + _t0 * _d_j) * _t2 + _t1 * 0. + (_d_j * j + j * _d_j) * _t4 + _t3 * 0.;
+  // CHECK-NEXT: }
+  
+  // CHECK: double memFn_darg0(double i, double j) {
+  // CHECK-NEXT:     double _d_i = 1;
+  // CHECK-NEXT:     double _d_j = 0;
+  // CHECK-NEXT:     double _t0 = i * i;
+  // CHECK-NEXT:     double _t1 = _t0 * j;
+  // CHECK-NEXT:     double &_t2 = this->x;
+  // CHECK-NEXT:     double _t3 = j * j;
+  // CHECK-NEXT:     double &_t4 = this->y;
+  // CHECK-NEXT:     return ((_d_i * i + i * _d_i) * j + _t0 * _d_j) * _t2 + _t1 * 0. + (_d_j * j + j * _d_j) * _t4 + _t3 * 0.;
+  // CHECK-NEXT: }
+  
+  // CHECK: double memFn_darg0(double i, double j) {
+  // CHECK-NEXT:     double _d_i = 1;
+  // CHECK-NEXT:     double _d_j = 0;
+  // CHECK-NEXT:     double _t0 = i * i;
+  // CHECK-NEXT:     double _t1 = _t0 * j;
+  // CHECK-NEXT:     double &_t2 = this->x;
+  // CHECK-NEXT:     double _t3 = j * j;
+  // CHECK-NEXT:     double &_t4 = this->y;
+  // CHECK-NEXT:     return ((_d_i * i + i * _d_i) * j + _t0 * _d_j) * _t2 + _t1 * 0. + (_d_j * j + j * _d_j) * _t4 + _t3 * 0.;
+  // CHECK-NEXT: }
+  
+  // CHECK: double memFn_darg0(double i, double j) {
+  // CHECK-NEXT:     double _d_i = 1;
+  // CHECK-NEXT:     double _d_j = 0;
+  // CHECK-NEXT:     double _t0 = i * i;
+  // CHECK-NEXT:     double _t1 = _t0 * j;
+  // CHECK-NEXT:     double &_t2 = this->x;
+  // CHECK-NEXT:     double _t3 = j * j;
+  // CHECK-NEXT:     double &_t4 = this->y;
+  // CHECK-NEXT:     return ((_d_i * i + i * _d_i) * j + _t0 * _d_j) * _t2 + _t1 * 0. + (_d_j * j + j * _d_j) * _t4 + _t3 * 0.;
+  // CHECK-NEXT: }
+  
+  // CHECK: double memFn_darg0(double i, double j) {
+  // CHECK-NEXT:     double _d_i = 1;
+  // CHECK-NEXT:     double _d_j = 0;
+  // CHECK-NEXT:     double _t0 = i * i;
+  // CHECK-NEXT:     double _t1 = _t0 * j;
+  // CHECK-NEXT:     double &_t2 = this->x;
+  // CHECK-NEXT:     double _t3 = j * j;
+  // CHECK-NEXT:     double &_t4 = this->y;
+  // CHECK-NEXT:     return ((_d_i * i + i * _d_i) * j + _t0 * _d_j) * _t2 + _t1 * 0. + (_d_j * j + j * _d_j) * _t4 + _t3 * 0.;
+  // CHECK-NEXT: }
+  
+  // CHECK: double memFn_darg0(double i, double j) {
+  // CHECK-NEXT:     double _d_i = 1;
+  // CHECK-NEXT:     double _d_j = 0;
+  // CHECK-NEXT:     double _t0 = i * i;
+  // CHECK-NEXT:     double _t1 = _t0 * j;
+  // CHECK-NEXT:     double &_t2 = this->x;
+  // CHECK-NEXT:     double _t3 = j * j;
+  // CHECK-NEXT:     double &_t4 = this->y;
+  // CHECK-NEXT:     return ((_d_i * i + i * _d_i) * j + _t0 * _d_j) * _t2 + _t1 * 0. + (_d_j * j + j * _d_j) * _t4 + _t3 * 0.;
+  // CHECK-NEXT: }
+  
+  // CHECK: double memFn_darg0(double i, double j) {
+  // CHECK-NEXT:     double _d_i = 1;
+  // CHECK-NEXT:     double _d_j = 0;
+  // CHECK-NEXT:     double _t0 = i * i;
+  // CHECK-NEXT:     double _t1 = _t0 * j;
+  // CHECK-NEXT:     double &_t2 = this->x;
+  // CHECK-NEXT:     double _t3 = j * j;
+  // CHECK-NEXT:     double &_t4 = this->y;
+  // CHECK-NEXT:     return ((_d_i * i + i * _d_i) * j + _t0 * _d_j) * _t2 + _t1 * 0. + (_d_j * j + j * _d_j) * _t4 + _t3 * 0.;
+  // CHECK-NEXT: }  
 };
 
 double nonMemFn(double i,double j) {
@@ -161,6 +247,63 @@ double nonMemFn(double i,double j) {
 // CHECK-NEXT:     return ((_d_i * i + i * _d_i) * j + _t0 * _d_j) * j + _t1 * _d_j;
 // CHECK-NEXT: }
 
+// CHECK: double nonMemFn_darg0(double i, double j) {
+// CHECK-NEXT:     double _d_i = 1;
+// CHECK-NEXT:     double _d_j = 0;
+// CHECK-NEXT:     double _t0 = i * i;
+// CHECK-NEXT:     double _t1 = _t0 * j;
+// CHECK-NEXT:     return ((_d_i * i + i * _d_i) * j + _t0 * _d_j) * j + _t1 * _d_j;
+// CHECK-NEXT: }
+
+// CHECK: double nonMemFn_darg0(double i, double j) {
+// CHECK-NEXT:     double _d_i = 1;
+// CHECK-NEXT:     double _d_j = 0;
+// CHECK-NEXT:     double _t0 = i * i;
+// CHECK-NEXT:     double _t1 = _t0 * j;
+// CHECK-NEXT:     return ((_d_i * i + i * _d_i) * j + _t0 * _d_j) * j + _t1 * _d_j;
+// CHECK-NEXT: }
+
+// CHECK: double nonMemFn_darg0(double i, double j) {
+// CHECK-NEXT:     double _d_i = 1;
+// CHECK-NEXT:     double _d_j = 0;
+// CHECK-NEXT:     double _t0 = i * i;
+// CHECK-NEXT:     double _t1 = _t0 * j;
+// CHECK-NEXT:     return ((_d_i * i + i * _d_i) * j + _t0 * _d_j) * j + _t1 * _d_j;
+// CHECK-NEXT: }
+
+// CHECK: double nonMemFn_darg0(double i, double j) {
+// CHECK-NEXT:     double _d_i = 1;
+// CHECK-NEXT:     double _d_j = 0;
+// CHECK-NEXT:     double _t0 = i * i;
+// CHECK-NEXT:     double _t1 = _t0 * j;
+// CHECK-NEXT:     return ((_d_i * i + i * _d_i) * j + _t0 * _d_j) * j + _t1 * _d_j;
+// CHECK-NEXT: }
+
+// CHECK: double nonMemFn_darg0(double i, double j) {
+// CHECK-NEXT:     double _d_i = 1;
+// CHECK-NEXT:     double _d_j = 0;
+// CHECK-NEXT:     double _t0 = i * i;
+// CHECK-NEXT:     double _t1 = _t0 * j;
+// CHECK-NEXT:     return ((_d_i * i + i * _d_i) * j + _t0 * _d_j) * j + _t1 * _d_j;
+// CHECK-NEXT: }
+
+// CHECK: double nonMemFn_darg0(double i, double j) {
+// CHECK-NEXT:     double _d_i = 1;
+// CHECK-NEXT:     double _d_j = 0;
+// CHECK-NEXT:     double _t0 = i * i;
+// CHECK-NEXT:     double _t1 = _t0 * j;
+// CHECK-NEXT:     return ((_d_i * i + i * _d_i) * j + _t0 * _d_j) * j + _t1 * _d_j;
+// CHECK-NEXT: }
+
+// CHECK: double nonMemFn_darg0(double i, double j) {
+// CHECK-NEXT:     double _d_i = 1;
+// CHECK-NEXT:     double _d_j = 0;
+// CHECK-NEXT:     double _t0 = i * i;
+// CHECK-NEXT:     double _t1 = _t0 * j;
+// CHECK-NEXT:     return ((_d_i * i + i * _d_i) * j + _t0 * _d_j) * j + _t1 * _d_j;
+// CHECK-NEXT: }
+
+
 #define MEM_FN_TEST(var)\
 printf("%.2f\n",var.execute(expr,7,9));\
 printf("%.2f\n",var.execute(expr,11,9));
@@ -180,15 +323,26 @@ int main() {
   auto memFnIndirectIndirectPtr = memFnIndirectPtr;
 
   auto d_memFnPtr = clad::differentiate(memFnPtr,"i");
+  auto d_memFnPtrPar = clad::differentiate((memFnPtr),"i");
   auto d_memFnPtrToPtr = clad::differentiate(*memFnPtrToPtr,"i");
   auto d_memFnPtrToPtr_1 = clad::differentiate(**&memFnPtrToPtr,"i");
+  auto d_memFnPtrToPtr_1Par = clad::differentiate(**(&memFnPtrToPtr),"i");
+  auto d_memFnPtrToPtr_1ParPar = clad::differentiate(*(*(&memFnPtrToPtr)),"i");
   auto d_memFnPtrToPtrToPtr = clad::differentiate(**memFnPtrToPtrToPtr,"i");
+  auto d_memFnPtrToPtrToPtrPar = clad::differentiate(*(*memFnPtrToPtrToPtr),"i");
+  auto d_memFnPtrToPtrToPtrParPar = clad::differentiate(((*(*memFnPtrToPtrToPtr))),"i");
   auto d_memFnPtrToPtrToPtr_1 = clad::differentiate(***&memFnPtrToPtrToPtr,"i");
+  auto d_memFnPtrToPtrToPtr_1Par = clad::differentiate(***(&memFnPtrToPtrToPtr),"i");
+  auto d_memFnPtrToPtrToPtr_1ParPar = clad::differentiate(**(*(&memFnPtrToPtrToPtr)),"i");
+  auto d_memFnPtrToPtrToPtr_1ParParPar = clad::differentiate(*(*(*(&memFnPtrToPtrToPtr))),"i");
   auto d_memFnIndirectPtr = clad::differentiate(memFnIndirectPtr,"i");
   auto d_memFnIndirectIndirectPtr = clad::differentiate(memFnIndirectIndirectPtr,"i");
 
-  MEM_FN_TEST(d_memFnPtr);  // CHECK-EXEC: 630.00
-                            // CHECK-EXEC: 990.00
+  MEM_FN_TEST(d_memFnPtr); // CHECK-EXEC: 630.00
+                           // CHECK-EXEC: 990.00
+
+  MEM_FN_TEST(d_memFnPtrPar); // CHECK-EXEC: 630.00
+                              // CHECK-EXEC: 990.00
 
   MEM_FN_TEST(d_memFnPtrToPtr); // CHECK-EXEC: 630.00
                                 // CHECK-EXEC: 990.00
@@ -196,17 +350,40 @@ int main() {
   MEM_FN_TEST(d_memFnPtrToPtr_1); // CHECK-EXEC: 630.00
                                   // CHECK-EXEC: 990.00
 
-  MEM_FN_TEST(d_memFnPtrToPtrToPtr);  // CHECK-EXEC: 630.00
-                                      // CHECK-EXEC: 990.00
+  MEM_FN_TEST(d_memFnPtrToPtr_1Par); // CHECK-EXEC: 630.00
+                                     // CHECK-EXEC: 990.00
 
-  MEM_FN_TEST(d_memFnPtrToPtrToPtr_1);  // CHECK-EXEC: 630.00
+  MEM_FN_TEST(d_memFnPtrToPtr_1ParPar); // CHECK-EXEC: 630.00
                                         // CHECK-EXEC: 990.00
 
-  MEM_FN_TEST(d_memFnIndirectPtr);  // CHECK-EXEC: 630.00
-                                    // CHECK-EXEC: 990.00
+  MEM_FN_TEST(d_memFnPtrToPtrToPtr); // CHECK-EXEC: 630.00
+                                     // CHECK-EXEC: 990.00
 
-  MEM_FN_TEST(d_memFnIndirectIndirectPtr);  // CHECK-EXEC: 630.00
-                                            // CHECK-EXEC: 990.00
+  MEM_FN_TEST(d_memFnPtrToPtrToPtrPar); // CHECK-EXEC: 630.00
+                                        // CHECK-EXEC: 990.00
+
+  MEM_FN_TEST(d_memFnPtrToPtrToPtrParPar); // CHECK-EXEC: 630.00
+                                           // CHECK-EXEC: 990.00
+
+  MEM_FN_TEST(d_memFnPtrToPtrToPtr_1); // CHECK-EXEC: 630.00
+                                       // CHECK-EXEC: 990.00
+
+  MEM_FN_TEST(d_memFnPtrToPtrToPtr_1Par); // CHECK-EXEC: 630.00
+                                          // CHECK-EXEC: 990.00
+
+  MEM_FN_TEST(d_memFnPtrToPtrToPtr_1ParPar); // CHECK-EXEC: 630.00
+                                             // CHECK-EXEC: 990.00
+
+  MEM_FN_TEST(d_memFnPtrToPtrToPtr_1ParParPar); // CHECK-EXEC: 630.00
+                                                // CHECK-EXEC: 990.00
+
+  MEM_FN_TEST(d_memFnIndirectPtr); // CHECK-EXEC: 630.00
+                                   // CHECK-EXEC: 990.00
+
+  MEM_FN_TEST(d_memFnIndirectIndirectPtr); // CHECK-EXEC: 630.00
+                                           // CHECK-EXEC: 990.00
+
+
 
   auto nonMemFnPtr = &nonMemFn;
   auto nonMemFnPtrToPtr = &nonMemFnPtr;
@@ -215,31 +392,59 @@ int main() {
   auto nonMemFnIndirectIndirectPtr = nonMemFnIndirectPtr;
 
   auto d_nonMemFn = clad::differentiate(nonMemFn,"i");
+  auto d_nonMemFnPar = clad::differentiate((nonMemFn),"i");
   auto d_nonMemFnPtr = clad::differentiate(nonMemFnPtr,"i");
   auto d_nonMemFnPtrToPtr = clad::differentiate(*nonMemFnPtrToPtr,"i");
+  auto d_nonMemFnPtrToPtrPar = clad::differentiate((*(nonMemFnPtrToPtr)),"i");
   auto d_nonMemFnPtrToPtr_1 = clad::differentiate(**&nonMemFnPtrToPtr,"i");
+  auto d_nonMemFnPtrToPtr_1Par = clad::differentiate(**(&nonMemFnPtrToPtr),"i");
+  auto d_nonMemFnPtrToPtr_1ParPar = clad::differentiate(*(*(&nonMemFnPtrToPtr)),"i");
   auto d_nonMemFnPtrToPtrToPtr = clad::differentiate(**nonMemFnPtrToPtrToPtr,"i");
   auto d_nonMemFnPtrToPtrToPtr_1 = clad::differentiate(***&nonMemFnPtrToPtrToPtr,"i");
+  auto d_nonMemFnPtrToPtrToPtr_1Par = clad::differentiate(***(&nonMemFnPtrToPtrToPtr),"i");
+  auto d_nonMemFnPtrToPtrToPtr_1ParPar = clad::differentiate(*(**(&nonMemFnPtrToPtrToPtr)),"i");
+  auto d_nonMemFnPtrToPtrToPtr_1ParParPar = clad::differentiate((*(**((&nonMemFnPtrToPtrToPtr)))),"i");
   auto d_nonMemFnIndirectPtr = clad::differentiate(nonMemFnIndirectPtr,"i");
   auto d_nonMemFnIndirectIndirectPtr = clad::differentiate(nonMemFnIndirectIndirectPtr,"i");
 
-  NON_MEM_FN_TEST(d_nonMemFn);  // CHECK-EXEC: 96.00
-                                // CHECK-EXEC: 350.00
+  NON_MEM_FN_TEST(d_nonMemFn); // CHECK-EXEC: 96.00
+                               // CHECK-EXEC: 350.00
+
+  NON_MEM_FN_TEST(d_nonMemFnPar); // CHECK-EXEC: 96.00
+                                  // CHECK-EXEC: 350.00
 
   NON_MEM_FN_TEST(d_nonMemFnPtr); // CHECK-EXEC: 96.00
                                   // CHECK-EXEC: 350.00
 
-  NON_MEM_FN_TEST(d_nonMemFnPtrToPtr);  // CHECK-EXEC: 96.00
-                                        // CHECK-EXEC: 350.00
+  NON_MEM_FN_TEST(d_nonMemFnPtrToPtr); // CHECK-EXEC: 96.00
+                                       // CHECK-EXEC: 350.00
 
-  NON_MEM_FN_TEST(d_nonMemFnPtrToPtr_1);  // CHECK-EXEC: 96.00
+  NON_MEM_FN_TEST(d_nonMemFnPtrToPtrPar); // CHECK-EXEC: 96.00
                                           // CHECK-EXEC: 350.00
+
+  NON_MEM_FN_TEST(d_nonMemFnPtrToPtr_1); // CHECK-EXEC: 96.00
+                                         // CHECK-EXEC: 350.00
+
+  NON_MEM_FN_TEST(d_nonMemFnPtrToPtr_1Par); // CHECK-EXEC: 96.00
+                                            // CHECK-EXEC: 350.00
+
+  NON_MEM_FN_TEST(d_nonMemFnPtrToPtr_1ParPar); // CHECK-EXEC: 96.00
+                                               // CHECK-EXEC: 350.00
 
   NON_MEM_FN_TEST(d_nonMemFnPtrToPtrToPtr); // CHECK-EXEC: 96.00
                                             // CHECK-EXEC: 350.00
 
   NON_MEM_FN_TEST(d_nonMemFnPtrToPtrToPtr_1); // CHECK-EXEC: 96.00
                                               // CHECK-EXEC: 350.00
+
+  NON_MEM_FN_TEST(d_nonMemFnPtrToPtrToPtr_1Par); // CHECK-EXEC: 96.00
+                                                 // CHECK-EXEC: 350.00
+
+  NON_MEM_FN_TEST(d_nonMemFnPtrToPtrToPtr_1ParPar); // CHECK-EXEC: 96.00
+                                                    // CHECK-EXEC: 350.00
+
+  NON_MEM_FN_TEST(d_nonMemFnPtrToPtrToPtr_1ParParPar); // CHECK-EXEC: 96.00
+                                                       // CHECK-EXEC: 350.00
 
   NON_MEM_FN_TEST(d_nonMemFnIndirectPtr); // CHECK-EXEC: 96.00
                                           // CHECK-EXEC: 350.00
