2759bee00e6ed6595241e8789c6bbd1cd415e74c
Parth <partharora99160808@gmail.com>
2022-03-20 01:17:23 +0530

    Update scripts

--- scripts/build_clad.sh
+++ scripts/build_clad.sh
@@ -2,5 +2,5 @@ mkdir -p /workspace/builds/build-${1}
 cd /workspace/builds/build-10
 mkdir obj inst
 cd obj
-cmake -G Ninja ../../../clad -DClang_DIR=/usr/lib/llvm-${10} -DLLVM_DIR=/usr/lib/llvm-${1} -DCMAKE_INSTALL_PREFIX=../inst -DLLVM_EXTERNAL_LIT="`which lit`"
+cmake -G Ninja ../../../clad -DClang_DIR=/usr/lib/llvm-${1} -DLLVM_DIR=/usr/lib/llvm-${1} -DCMAKE_INSTALL_PREFIX=../inst -DLLVM_EXTERNAL_LIT="`which lit`"
 ninja install
